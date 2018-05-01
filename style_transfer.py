e6bc62dc81389480068358973deb62b6c4add187
Goutham <bhattagoutham@gmail.com>
2018-05-01 07:39:30 +0530

    added command line args

--- style_transfer.py
+++ style_transfer.py
@@ -1,11 +1,18 @@
 import numpy as np
 import cv2
+import argparse
 import tensorflow as tf
 import keras.backend as K
 from keras.models import Model
 from keras.layers import Input, Conv2D, UpSampling2D, Activation, Lambda, MaxPooling2D
 import torchfile
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--content-path', type=str, dest='content_path', help='Content image or folder of images')
+parser.add_argument('--style-path', type=str, dest='style_path', help='Style image or folder of images')
+parser.add_argument('--alpha', type=float, help="Alpha blend value", default=0.9)
+parser.add_argument('--out-path', type=str, dest='out_path', help='Output folder path')
+args = parser.parse_args()
 
 def disp_img(img, name):
     cv2.imshow(name,img)
@@ -41,6 +48,8 @@ def wct_np(content, style, alpha=0.6, eps=1e-5):
 
     Dc = np.diag((wc[:k_c]+eps)**-0.5)
 
+    # fc_hat = Dc.dot(Ec[:,:k_c]).dot(fc)
+
     fc_hat = Ec[:,:k_c].dot(Dc).dot(Ec[:,:k_c].T).dot(fc)
 
     ms = style_flat.mean(axis=1, keepdims=True)
@@ -53,6 +62,8 @@ def wct_np(content, style, alpha=0.6, eps=1e-5):
     k_s = (ws > 1e-5).sum()
     
     Ds = np.sqrt(np.diag(ws[:k_s]+eps))
+
+    # fcs_hat = Ds.dot(Es[:,:k_s]).dot(fc_hat)
 
     fcs_hat = Es[:,:k_s].dot(Ds).dot(Es[:,:k_s].T).dot(fc_hat)
 
@@ -142,82 +153,88 @@ def vgg_from_t7(t7_file, net_type, layer, target_layer=None):
     return model
 
 
+def stylize(content_path, style_path, out_path, alpha):
 
-# path to vgg_normalised_conv#_#.t7 and  feature_invertor_conv#_#.t7 files
-encoder_path = "/home/goutham/WCT-TF-master/models2/models"
-decoder_path = "/home/goutham/WCT-TF-master/models2/models"
-encoder = []
-decoder = []
+    # path to vgg_normalised_conv#_#.t7 and  feature_invertor_conv#_#.t7 files
+    encoder_path = "/home/goutham/WCT-TF-master/models2/models"
+    decoder_path = "/home/goutham/WCT-TF-master/models2/models"
+    encoder = []
+    decoder = []
 
-for i in range(5):
-    l = str(i+1)
-    encoder.append(vgg_from_t7(encoder_path+'/vgg_normalised_conv'+l+'_1'+'.t7', "encoder", i+1))
-    decoder.append(vgg_from_t7(decoder_path+'/feature_invertor_conv'+l+'_1'+'.t7', "decoder", i+1))
-    print()
+    for i in range(5):
+        l = str(i+1)
+        encoder.append(vgg_from_t7(encoder_path+'/vgg_normalised_conv'+l+'_1'+'.t7', "encoder", i+1))
+        decoder.append(vgg_from_t7(decoder_path+'/feature_invertor_conv'+l+'_1'+'.t7', "decoder", i+1))
+        print()
 
 
-style_path = '2.jpg'
-content_path = 'jes.jpeg'
+    # style_path = '2.jpg'
+    # content_path = '21.jpg'
 
-style = cv2.imread(style_path)
-content = cv2.imread(content_path)
+    style = cv2.imread(style_path)
+    content = cv2.imread(content_path)
 
-# disp_img(content, "content")
-# disp_img(style, "style")
+    # disp_img(content, "content")
+    # disp_img(style, "style")
 
-# tried this, but no big difference
-# style = cv2.cvtColor(style, cv2.COLOR_BGR2HSV)
-# content = cv2.cvtColor(content, cv2.COLOR_BGR2HSV)
+    # tried this, but no big difference
+    # style = cv2.cvtColor(style, cv2.COLOR_BGR2HSV)
+    # content = cv2.cvtColor(content, cv2.COLOR_BGR2HSV)
 
-print("content size: ", content.shape)
-print("style size: ", style.shape)
+    print("content size: ", content.shape)
+    print("style size: ", style.shape)
 
-r,c,p = style.shape
-style = style.reshape(1,r,c,p)
+    r,c,p = style.shape
+    style = style.reshape(1,r,c,p)
 
-print("\n*******Begin Universal Style Transfer*******\n")
+    print("\n*******Begin Universal Style Transfer*******\n")
 
-alpha = 0.9
-iters = 1
-relu = 5
+    # alpha = 0.9
+    iters = 1
+    relu = 5
 
-for i in range(iters):
+    for i in range(iters):
 
-    i = relu-i-1
-    print("\nLevel ",i+1," : \n-----------")
+        i = relu-i-1
+        print("\nLevel ",i+1," : \n-----------")
+        
+        r,c,p = content.shape
+        content = content.reshape(1,r,c,p)
+
+        content_features = encoder[i].predict(content)
+        style_features = encoder[i].predict(style)
+
+        print("content_features: ", content_features.shape)
+        print("style_features: ", style_features.shape)
+
+        s,r,c,p = content_features.shape
+        disp_cont_feat = content_features.reshape(r*c, p)
+
+        s,r,c,p = style_features.shape
+        disp_style_feat = style_features.reshape(r*c, p)
+
+        # disp_img(disp_cont_feat, "cont_feat")
+        # disp_img(disp_style_feat, "style_feat")
+
+        blended = wct_np(content_features, style_features, alpha, eps=0)
+        print("blended_features: ", blended.shape)
+
+
+        out = decoder[i].predict(blended)
+        s,r,c,p = out.shape
+        content = out.reshape(r,c,p)
+
+        disp_img(np.uint8(content), "Level_"+str(i))
+        print("reconstructed_content_shape: ", content.shape)
+
+
     
-    r,c,p = content.shape
-    content = content.reshape(1,r,c,p)
-
-    content_features = encoder[i].predict(content)
-    style_features = encoder[i].predict(style)
-
-    print("content_features: ", content_features.shape)
-    print("style_features: ", style_features.shape)
-
-    s,r,c,p = content_features.shape
-    disp_cont_feat = content_features.reshape(r*c, p)
-
-    s,r,c,p = style_features.shape
-    disp_style_feat = style_features.reshape(r*c, p)
-
-    # disp_img(disp_cont_feat, "cont_feat")
-    # disp_img(disp_style_feat, "style_feat")
-
-    blended = wct_np(content_features, style_features, alpha, eps=1e-5)
-    print("blended_features: ", blended.shape)
-
-
-    out = decoder[i].predict(blended)
-    s,r,c,p = out.shape
-    content = out.reshape(r,c,p)
-
-    disp_img(np.uint8(content), "Level_"+str(i))
-    print("reconstructed_content_shape: ", content.shape)
+    out_file = content_path[content_path.rfind('/'):content_path.find('.')]+"_"+style_path[style_path.rfind('/')+1:style_path.find('.')]+".png"
+    # disp_img(np.uint8(content), "result")
+    # out_file = content_path[:content_path.find('.')]+"_"+style_path[:style_path.find('.')]+"_"+"relu_"+str(relu)+"_alpha_"+str(alpha)+"_iters_"+str(iters)+".png"
+    
+    cv2.imwrite(out_path+"/"+out_file, np.uint8(content))
 
 
 
-# disp_img(np.uint8(content), "result")
-out_file = content_path[:content_path.find('.')]+"_"+style_path[:style_path.find('.')]+"_"+"relu_"+str(relu)+"_alpha_"+str(alpha)+"_iters_"+str(iters)+".png"
-cv2.imwrite(out_file, np.uint8(content))
-
+stylize(args.content_path, args.style_path, args.out_path, args.alpha)
